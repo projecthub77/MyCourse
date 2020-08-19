4f25a60232289072ff72b46d2ae7552300ee6c56
Marco <marco.iudex@gmail.com>
2020-08-19 09:29:50 +0200

    Commit 3 prime viste

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -5,11 +5,11 @@ namespace MyCourse.Controllers
     public class CoursesController : Controller
     {
         public IActionResult Index(){
-            return   Content("Sono Index");        
+            return   View();        
         }
 
         public IActionResult Detail(int id){
-            return   Content($"Sono il dettaglio Index {id}");        
+            return   View();        
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -17,9 +17,8 @@ namespace MyCourse
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(options =>
-            options.EnableEndpointRouting = false
-                ) ;
+            services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,18 +33,13 @@ namespace MyCourse
 
             app.UseRouting();
 
-            app.UseMvcWithDefaultRoute();
-            //app.UseMvc(routeBuilder =>
-            //{
-            //    routeBuilder.MapRoute("default", "{controller=Home}/{action=Index}/{id}");
-            //});
+
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
